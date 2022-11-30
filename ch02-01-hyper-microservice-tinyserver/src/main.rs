f7c11483c396cf4854be82ad7f2b0b1d8f9893a7
Laris Qiao <laris.qiao@gmail.com>
2022-11-30 23:09:59 +0800

    update ch02 01 tinyserver

--- ch02-01-hyper-microservice-tinyserver/src/main.rs
+++ ch02-01-hyper-microservice-tinyserver/src/main.rs
@@ -1,15 +1,20 @@
-use hyper::{Body, Response, Server};
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use hyper::{Body, Response, Server, Error};
+use hyper::service::{make_service_fn, service_fn};
+// curl -viA '' http://localhost:8080
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let addr = ([127, 0, 0, 1], 8080).into();
-    let builder = Server::bind(&addr);
-    let server = builder.serve(|| {
-        service_fn_ok(|_| {
-            Response::new(Body::from("Rust Microservice"))
-        })
+    let make_service = make_service_fn(|_| async move {
+        Ok::<_, Error>(service_fn(|req| async move {
+            println!("{:#?}", req);
+            let resp = Response::new(Body::from("Rust Microservice"));
+            println!("{:#?}", resp);
+            Ok::<_, Error>(resp)
+        }))
     });
-    let server = server.map_err(drop);
-    hyper::rt::run(server);
+    let server = Server::bind(&addr).serve(make_service);
+    if let Err(e) = server.await {
+        eprintln!("Server error: {}", e);
+    }
 }
